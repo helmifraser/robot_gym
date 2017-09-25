eb6935c893c1975876ed21c224e0e8c44af91dfa
helmifraser <helmifraser1@gmail.com>
2017-09-25 20:05:15 +0100

    Added env reset topic

--- turtlebot.py
+++ turtlebot.py
@@ -7,7 +7,10 @@ import numpy
 import random
 import time
 import rospy
+import os
+import signal
 from rosgraph_msgs.msg import Log
+from std_msgs.msg import Bool
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -24,6 +27,8 @@ class TurtlebotLearning(object):
         self.rate = rospy.Rate(self.frequency)
         self.validation = "empty"
         self.rosout_sub = rospy.Subscriber('/rosout', Log, self.rosout_cb)
+        self.reset_sub = rospy.Subscriber('/reset_env', Bool, self.reset_env_cb)
+        self.pid = os.getpid()
 
         self.wait_for_gym()
 
@@ -34,16 +39,31 @@ class TurtlebotLearning(object):
         """
 
         rospy.logwarn("Shutting down all the things fam")
+        # os.kill(self.pid, signal.SIGKILL)
 
     def wait_for_gym(self):
+        """
+            I know, I know it's so hacky it's disgusting
+        """
+
         completed = False
         while completed is False:
             if "GazeboRosKobuki" in self.validation:
                 completed = True
                 rospy.logwarn("Successfully initialised, ready to learn! Run 'gzclient' in another terminal")
 
-    def rosout_cb(self, data):
-        self.validation = data.msg
+    def rosout_cb(self, msg):
+        self.validation = msg.msg
+
+    def reset_env_cb(self, msg):
+        if msg.data == True:
+            self.env.reset()
+
+    def render_env(self):
+        """
+            This causes Gazebo to pop up, don't use it
+        """
+        self.env.render()
 
     def return_rate(self):
         return self.rate
