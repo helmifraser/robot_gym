bb4b36e5c5544ab47652e86e1c0691dbef2ee37c
helmifraser <helmifraser1@gmail.com>
2017-10-21 17:26:33 +0100

    Added hokuyo laser to simulation NOTE: must copy new files into system package

--- src/turtlebot.py
+++ src/turtlebot.py
@@ -21,9 +21,9 @@ class TurtlebotLearning(object):
         self.frequency = 10
         self.rate = rospy.Rate(self.frequency)
 
-        self.cmd_vel_pub_ = rospy.Publisher('/mobile_base/commands/velocity', geometry_msgs.msg.Twist, queue_size=10)
+        self.cmd_vel_pub_ = rospy.Publisher('/mobile_base/commands/velocity', geometry_msgs.msg.Twist, queue_size=1)
 
-        self.laser_scan_sub_ = rospy.Subscriber('/scan', sensor_msgs.msg.LaserScan, self.scan_cb)
+        self.laser_scan_sub_ = rospy.Subscriber('/laserscan', sensor_msgs.msg.LaserScan, self.scan_cb)
 
         self.laser_msg = sensor_msgs.msg.LaserScan()
         self.scan_angle_min = 0
@@ -32,6 +32,7 @@ class TurtlebotLearning(object):
         self.scan_range_min = 0
         self.scan_range_max = 0
 
+        self.cmd_msg = self.create_zeroed_twist()
         self.segmented_laser_data = [0, 0, 0, 0, 0, 0, 0]
 
         self.controller = mlp.MLP_NeuralNetwork(input=input_nodes,hidden=hidden_nodes,output=output_nodes)
@@ -46,6 +47,18 @@ class TurtlebotLearning(object):
         rospy.logwarn("Shutting down all the things fam")
         # os.kill(self.pid, signal.SIGKILL)
 
+    def drive(self, segmented_laser_data):
+        # if math.isnan(segmented_laser_data)
+        self.cmd_msg.linear.x = (segmented_laser_data[0] + segmented_laser_data[3] + segmented_laser_data[6])/(self.laser_msg.range_max + 10)
+        self.cmd_msg.angular.z = (segmented_laser_data[6] - segmented_laser_data[0])/(self.laser_msg.range_max + 10)
+        rospy.loginfo("linear x: {} angular z: {}".format(self.cmd_msg.linear.x, self.cmd_msg.angular.z))
+
+        if math.isnan(self.cmd_msg.linear.x) or math.isnan(self.cmd_msg.angular.z):
+            return
+
+        self.publish_vel(self.cmd_msg)
+        # if segmented_laser_data[0] < 0.5:
+
     def create_zeroed_twist(self):
         msg = geometry_msgs.msg.Twist()
         msg.linear.x = 0
@@ -59,28 +72,33 @@ class TurtlebotLearning(object):
 
     def scan_cb(self, msg):
         self.laser_msg = msg
-        if msg.ranges[0] is 'nan':
+
+        if math.isnan(msg.ranges[0]):
             self.segmented_laser_data[0] = msg.range_max + 10
         else:
             self.segmented_laser_data[0] = msg.ranges[0]
 
-        for i in range(91, len(msg.ranges) - 91, 91):
+        inc = int(math.floor(len(msg.ranges)/7))
+        # print(inc)
+        for i in range(inc, len(msg.ranges) - inc, inc):
             try:
-                if msg.ranges[i] is 'nan':
-                    rospy.logerr("shit is nan {}".format(i))
-                    self.segmented_laser_data[int(i/91)] = msg.range_max + 10
+                if math.isnan(msg.ranges[i]):
+                    # rospy.logerr("shit is nan {}".format(i))
+                    self.segmented_laser_data[int(i/inc)] = msg.range_max + 10
                 else:
-                    rospy.logerr("shit is not nan {}".format(i))
-                    self.segmented_laser_data[int(i/91)] = msg.ranges[i]
+                    # rospy.logerr("shit is not nan {}".format(i))
+                    self.segmented_laser_data[int(i/inc)] = msg.ranges[i]
             except Exception as e:
                 rospy.logerr(i)
 
-        if msg.ranges[6] is 'nan':
+        if math.isnan(msg.ranges[inc*7]):
+            # rospy.logerr("shit is nan {}".format(i))
             self.segmented_laser_data[6] = msg.range_max + 10
         else:
-            self.segmented_laser_data[6] = msg.ranges[637]
-
-        rospy.logwarn("msg: {}".format(msg.ranges[637]))
+            # rospy.logerr("shit is not nan {}".format(i))
+            self.segmented_laser_data[6] = msg.ranges[inc*7]
+#
+        # rospy.logwarn("msg: {}".format(msg.ranges[inc*7]))
 
     def extract_scan_params(self):
         self.scan_angle_min = self.laser_msg.angle_min
@@ -96,6 +114,9 @@ class TurtlebotLearning(object):
         return self.scan_angle_min, self.scan_angle_max, self.scan_angle_increment
 
     def return_laser_msg(self):
+        return self.laser_msg
+
+    def return_segmented_laser_data(self):
         return self.segmented_laser_data
 
     def return_rate(self):
@@ -128,8 +149,12 @@ def main():
         twist_msg.linear.x = commands[0]
         twist_msg.angular.z = commands[1]
 
+        hello = "yuo"
+        start = input('Enter anything to start: ')
+        print("all good fam", start)
 
         while rospy.is_shutdown() is not True:
+
             # instance.test()
             # instance.publish_vel(twist_msg)
             # instance.extract_scan_params()
@@ -138,8 +163,10 @@ def main():
             # angle_max = int(angle_max*180/math.pi)
             # angle_increment = angle_increment*180/math.pi
             # print("min: {}, max: {}, inc: {}".format(angle_min, angle_max, angle_increment))
-            laser_msg = instance.return_laser_msg()
+
+            laser_msg = instance.return_segmented_laser_data()
             print("vals: {}".format(laser_msg))
+            instance.drive(laser_msg)
             node_rate.sleep()
 
     except KeyboardInterrupt:
