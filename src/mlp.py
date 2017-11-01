09be36a50c3b5063ccc4f55d5dc0bbe02e30aa85
helmifraser <helmifraser1@gmail.com>
2017-11-01 21:06:38 +0000

    Now using leaky RELU as activation function, with linear on outputs

--- src/mlp.py
+++ src/mlp.py
@@ -11,57 +11,32 @@ class MLP_NeuralNetwork(object):
         self.input = input + 1 # add 1 for bias node
         self.hidden = hidden
         self.output = output
-        # set up array of 1s for activations
-        self.ai = [1.0] * self.input
-        self.ah = [1.0] * self.hidden
-        self.ao = [1.0] * self.output
         # create randomized weights
-        self.wi = np.random.randn(self.input, self.hidden)
-        self.wo = np.random.randn(self.hidden, self.output)
-        # create arrays of 0 for changes
-        self.ci = np.zeros((self.input, self.hidden))
-        self.co = np.zeros((self.hidden, self.output))
+        # self.wi = np.random.randn(self.input, self.hidden)
+        # self.wo = np.random.randn(self.hidden, self.output)
+        self.wi = np.zeros((self.input, self.hidden))
+        self.wo = np.zeros((self.hidden, self.output))
         self.time_vector = 0
-        self.time_loop = 0
 
     def tanh(self, x, derivative=False):
         if (derivative == True):
             return (1 - (x ** 2))
         return np.tanh(x)
 
-    def feedForward_old(self, inputs):
-        t = time.time()
-        if len(inputs) != self.input-1:
-            raise ValueError('Wrong number of inputs!')
-        # input activations
-        for i in range(self.input -1): # -1 is to avoid the bias
-            self.ai[i] = inputs[i]
-        # hidden activations
-        for j in range(self.hidden):
-            sum = 0.0
-            for i in range(self.input):
-                sum += self.ai[i] * self.wi[i][j]
-            self.ah[j] = self.tanh(sum)
-        # output activations
-        for k in range(self.output):
-            sum = 0.0
-            for j in range(self.hidden):
-                sum += self.ah[j] * self.wo[j][k]
-            self.ao[k] = self.tanh(sum)
-        self.time_loop = time.time() - t
-        print("time: {}".format(self.time_loop))
-        return self.ao[:]
+    def leaky_relu(self, data, epsilon=0.01):
+        return np.maximum(data, epsilon * data)
 
     def feedForward(self, network_inputs):
-        t = time.time()
+        # t = time.time()
         input_activations = np.zeros(self.input)
         input_activations[:-1] = network_inputs
-        input_activations = self.tanh(input_activations)
+        input_activations = self.leaky_relu(input_activations)
         input_activations[-1] = 1
-        hidden_activations = self.tanh(np.dot(input_activations, self.wi))
-        output_activations = np.tanh(np.dot(hidden_activations, self.wo))
-        self.time_vector = time.time() - t
-        print("time: {}".format(self.time_vector))
+        hidden_activations = self.leaky_relu(np.dot(input_activations, self.wi))
+        output_activations = np.dot(hidden_activations, self.wo)
+        # self.time_vector = time.time() - t
+        # print("vector time: {}".format(self.time_vector))
+        return output_activations
 
     def change_weights(self, input_to_hidden, hidden_to_output):
         self.wi = input_to_hidden
@@ -70,11 +45,10 @@ class MLP_NeuralNetwork(object):
 def main():
 
     my_mlp = MLP_NeuralNetwork(input=8, hidden=16, output=2)
+    my_mlp.change_weights(input_to_hidden=np.random.randn(my_mlp.input, my_mlp.hidden), hidden_to_output=np.random.randn(my_mlp.hidden, my_mlp.output))
     mlp_in = np.random.randn(8)
-    print(mlp_in)
     output = my_mlp.feedForward(mlp_in)
-    out_old = my_mlp.feedForward_old(mlp_in)
-    # print(output)
+    print(output)
 
 if __name__ == '__main__':
     main()
