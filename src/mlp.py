325b91481cfd4e34e1d2565b856c70fee51fc75d
Helmi Fraser <helmifraser1@gmail.com>
2017-11-01 20:05:30 +0000

    Testing a rework of the feed forward function, eliminating for loops

--- src/mlp.py
+++ src/mlp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 class MLP_NeuralNetwork(object):
     def __init__(self, input, hidden, output):
@@ -16,18 +17,20 @@ class MLP_NeuralNetwork(object):
         self.ao = [1.0] * self.output
         # create randomized weights
         self.wi = np.random.randn(self.input, self.hidden)
-        print(np.shape(self.wi))
         self.wo = np.random.randn(self.hidden, self.output)
         # create arrays of 0 for changes
         self.ci = np.zeros((self.input, self.hidden))
         self.co = np.zeros((self.hidden, self.output))
+        self.time_vector = 0
+        self.time_loop = 0
 
     def tanh(self, x, derivative=False):
         if (derivative == True):
             return (1 - (x ** 2))
         return np.tanh(x)
 
-    def feedForward(self, inputs):
+    def feedForward_old(self, inputs):
+        t = time.time()
         if len(inputs) != self.input-1:
             raise ValueError('Wrong number of inputs!')
         # input activations
@@ -45,7 +48,20 @@ class MLP_NeuralNetwork(object):
             for j in range(self.hidden):
                 sum += self.ah[j] * self.wo[j][k]
             self.ao[k] = self.tanh(sum)
+        self.time_loop = time.time() - t
+        print("time: {}".format(self.time_loop))
         return self.ao[:]
+
+    def feedForward(self, network_inputs):
+        t = time.time()
+        input_activations = np.zeros(self.input)
+        input_activations[:-1] = network_inputs
+        input_activations = self.tanh(input_activations)
+        input_activations[-1] = 1
+        hidden_activations = self.tanh(np.dot(input_activations, self.wi))
+        output_activations = np.tanh(np.dot(hidden_activations, self.wo))
+        self.time_vector = time.time() - t
+        print("time: {}".format(self.time_vector))
 
     def change_weights(self, input_to_hidden, hidden_to_output):
         self.wi = input_to_hidden
@@ -55,8 +71,9 @@ def main():
 
     my_mlp = MLP_NeuralNetwork(input=8, hidden=16, output=2)
     mlp_in = np.random.randn(8)
-    # print(mlp_in)
+    print(mlp_in)
     output = my_mlp.feedForward(mlp_in)
+    out_old = my_mlp.feedForward_old(mlp_in)
     # print(output)
 
 if __name__ == '__main__':
