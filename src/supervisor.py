41257159c2fc4e427e107e6d269b9bac76517037
helmifraser <helmifraser1@gmail.com>
2017-11-03 16:55:47 +0000

    Now able to update sim time step programmatically through ROS service calls

--- src/supervisor.py
+++ src/supervisor.py
@@ -40,7 +40,7 @@ class Supervisor(object):
             reset()
             rospy.loginfo("Supervisor: Resetting world")
         except rospy.ServiceException, e:
-            rospy.loginfo("Supervisor: Service call failed: %s"%e)
+            rospy.loginfo("Supervisor: Service call (reset_world) failed: %s"%e)
 
     def pause_sim_physics(self):
         rospy.logwarn("Supervisor: Waiting for pause physics service...")
@@ -50,35 +50,42 @@ class Supervisor(object):
             pause()
             rospy.loginfo("Supervisor: Pausing physics")
         except rospy.ServiceException, e:
-            rospy.loginfo("Supervisor: Service call failed: %s"%e)
+            rospy.loginfo("Supervisor: Service call (pause_physics) failed: %s"%e)
 
     def set_physics_properties(self, properties):
         rospy.logwarn("Supervisor: Waiting for physics properties service...")
         rospy.wait_for_service("/gazebo/set_physics_properties")
+        time_step = properties.time_step
+        max_update_rate = properties.max_update_rate
+        gravity = properties.gravity
+        ode_config = properties.ode_config
+        # print("{}, {}, {}, {}".format(time_step, max_update_rate, gravity, ode_config))
         try:
-            set = rospy.ServiceProxy("/gazebo/set_physics_properties", properties)
-            set()
+            set = rospy.ServiceProxy("/gazebo/set_physics_properties", gazebo_msgs.srv.SetPhysicsProperties)
+            set(time_step, max_update_rate, gravity, ode_config)
             rospy.loginfo("Supervisor: Updating physics properties")
         except rospy.ServiceException, e:
-            rospy.loginfo("Supervisor: Service call failed: %s"%e)
+            rospy.loginfo("Supervisor: Service call (set_physics_properties) failed: %s"%e)
 
     def get_physics_properties(self):
         rospy.logwarn("Supervisor: Waiting for physics properties service...")
         rospy.wait_for_service("/gazebo/get_physics_properties")
         try:
-            properties = gazebo_msgs.srv.GetPhysicsProperties()
-            get = rospy.ServiceProxy("/gazebo/get_physics_properties", properties)
+            get = rospy.ServiceProxy("/gazebo/get_physics_properties", gazebo_msgs.srv.GetPhysicsProperties())
             rospy.loginfo("Supervisor: Getting physics properties")
-            return get
+            return get()
         except rospy.ServiceException, e:
-            rospy.loginfo("Supervisor: Service call failed: %s"%e)
+            rospy.loginfo("Supervisor: Service call (get_physics_properties) failed: %s"%e)
             return 0
 
     def update_time_step(self, new_time_step):
         current_properties = self.get_physics_properties()
         properties = self.convert_to_set(current_properties)
+
         properties.time_step = new_time_step
+        # print("update: \n {},\n {},\n {},\n {}".format(properties.time_step, properties.max_update_rate, properties.gravity, properties.ode_config))
         self.set_physics_properties(properties)
+        print("update_time_step: Updated time_step to {}".format(new_time_step))
 
     def convert_to_set(self, current_properties):
         properties = gazebo_msgs.srv.SetPhysicsProperties()
@@ -86,10 +93,7 @@ class Supervisor(object):
         properties.max_update_rate = current_properties.max_update_rate
         properties.gravity = current_properties.gravity
         properties.ode_config = current_properties.ode_config
-        # properties.time_step = 0
-        # properties.max_update_rate = current_properties.max_update_rate
-        # properties.gravity = current_properties.gravity
-        # properties.ode_config = current_properties.ode_config
+        # print("convert: \n {},\n {},\n {},\n {}".format(properties.time_step, properties.max_update_rate, properties.gravity, properties.ode_config))
         return properties
 
     def return_rate(self):
